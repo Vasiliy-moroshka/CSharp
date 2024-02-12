a0d22dc33b8920457b8e4bf06ea26328e253044f
Василий <Wh1lepp@gmail.com>
2024-02-12 16:22:51 +0100

    added task2:lesson03

---
+++ task11.3/Program.cs
@@ -0,0 +1,13 @@
+﻿void printSquares (int limit)
+{
+    int i = 1;
+    while(i <= limit)
+    {
+        Console.Write($"{i*i} ");
+        i++;
+    }
+}
+
+printSquares(5);
+printSquares(10);
+printSquares(15);
